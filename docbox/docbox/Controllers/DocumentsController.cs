b16f8dcc1c8405249a530c92b935fa357ee0b4f1
Kishan Maddula <emkishan@yahoo.com>
2012-11-06 01:26:54 -0700

    Build failed in prev commit

--- docbox/docbox/Controllers/DocumentsController.cs
+++ docbox/docbox/Controllers/DocumentsController.cs
@@ -294,53 +294,6 @@ namespace docbox.Controllers
 
 
 
-        public abstract class TempDataTransfer : ActionFilterAttribute
-        {
-            protected static readonly string Key = typeof(TempDataTransfer).FullName;
-        }
-
-        public class ExportToTempData : TempDataTransfer
-        {
-            public override void OnActionExecuted(ActionExecutedContext filterContext)
-            {
-                //Only export when ModelState is not valid
-                if (!filterContext.Controller.ViewData.ModelState.IsValid)
-                {
-                    //Export if we are redirecting
-                    if ((filterContext.Result is RedirectResult) || (filterContext.Result is RedirectToRouteResult))
-                    {
-                        filterContext.Controller.TempData[Key] = filterContext.Controller.ViewData.ModelState;
-                    }
-                }
-
-                base.OnActionExecuted(filterContext);
-            }
-        }
-
-        public class ImportFromTempData : TempDataTransfer
-        {
-            public override void OnActionExecuted(ActionExecutedContext filterContext)
-            {
-                ModelStateDictionary modelState = filterContext.Controller.TempData[Key] as ModelStateDictionary;
-
-                if (modelState != null)
-                {
-                    //Only Import if we are viewing
-                    if (filterContext.Result is ViewResult)
-                    {
-                        filterContext.Controller.ViewData.ModelState.Merge(modelState);
-                    }
-                    else
-                    {
-                        //Otherwise remove it.
-                        filterContext.Controller.TempData.Remove(Key);
-                    }
-                }
-
-                base.OnActionExecuted(filterContext);
-            }
-        }
-
         [HttpPost]
         [AcceptVerbs(HttpVerbs.Post), ExportToTempData]
         [Authorize(Roles = "employee,manager,ceo,vp")]
